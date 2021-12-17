ce9c8428abf5f31d48b7f986e3a4d2dc8211e905
LUENwu <1937839754@qq.com>
2021-12-17 18:02:29 +0800

    12-17

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ var server = http.createServer(function(request, response){
   var method = request.method
 
   /******** 从这里开始看，上面不要看 ************/
-  console.log("请求头:" )
-  console.log(request.headers)
-  console.log('method:')
-  console.log(method)
+  // console.log("请求头:" )
+  // console.log(request.headers)
+  // console.log('method:')
+  // console.log(method)
   
   // console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
   if(path === "/") {
